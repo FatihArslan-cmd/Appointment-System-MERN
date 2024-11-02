8caf35f764f9389d49b91805f8fd69d55e6447a0
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2024-11-02 20:45:59 +0300

    signup login screen uı done

--- src/router/App.js
+++ src/router/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from '../pages/LoginPage';
+import Register from '../pages/RegisterPage';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
+
+export default App;
