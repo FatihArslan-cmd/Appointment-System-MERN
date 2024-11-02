ac835cf87917ea1187b8a89de25f475daac36730
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2024-11-02 22:07:51 +0300

    bugs solved for auth

--- src/router/AuthContext.js
+++ src/router/AuthContext.js
@@ -1,9 +1,17 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(() => {
+    // Initialize state from local storage
+    const storedAuthState = localStorage.getItem('isAuthenticated');
+    return storedAuthState === 'true'; // Convert string to boolean
+  });
+
+  useEffect(() => {
+    localStorage.setItem('isAuthenticated', isAuthenticated);
+  }, [isAuthenticated]);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
